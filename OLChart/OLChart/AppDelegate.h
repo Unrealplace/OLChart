ff3abbc91ae2ff71ff93ddbb0aa795c8d0c23101
LiYang <ly@qiekj.com>
2019-05-15 10:50:50 +0800

    创建项目

---
+++ OLChart/OLChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OLChart
+//
+//  Created by LiYang on 2019/5/15.
+//  Copyright © 2019 LiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OLChart/OLChart/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  OLChart
+//
+//  Created by LiYang on 2019/5/15.
+//  Copyright © 2019 LiYang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
